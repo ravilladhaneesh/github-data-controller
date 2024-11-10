e6a3dd340334a776d2e2470130e8c0bddc1f21b1
ravillagopinath <ravilladhaneesh@gmail.com>
2024-11-10 22:41:06 +0530

    Add api gateway to invoke get data lambda function

---
+++ terraform/api-gateway.tf
@@ -0,0 +1,55 @@
+resource "aws_api_gateway_rest_api" "test-api-tf" {
+  name = "tf-test-api"
+}
+
+resource "aws_api_gateway_resource" "test-api-res-getdata" {
+  parent_id   = aws_api_gateway_rest_api.test-api-tf.root_resource_id
+  path_part   = "getData"
+  rest_api_id = aws_api_gateway_rest_api.test-api-tf.id
+}
+
+resource "aws_api_gateway_method" "get-api" {
+  authorization = "NONE"
+  http_method   = "GET"
+  resource_id   = aws_api_gateway_resource.test-api-res-getdata.id
+  rest_api_id   = aws_api_gateway_rest_api.test-api-tf.id
+}
+
+resource "aws_api_gateway_integration" "getdata-intergration" {
+  rest_api_id             = aws_api_gateway_rest_api.test-api-tf.id
+  resource_id             = aws_api_gateway_resource.test-api-res-getdata.id
+  http_method             = aws_api_gateway_method.get-api.http_method
+  integration_http_method = "POST"
+  type                    = "AWS"
+  uri                     = aws_lambda_function.lambda-get.invoke_arn
+
+  request_templates = {
+    "application/json" = <<EOF
+    #set($inputRoot = $input.path('$'))
+    {
+     "username": "$input.params('username')" 
+    }
+  EOF
+  }
+}
+
+resource "aws_api_gateway_method_response" "getdata-response_200" {
+  rest_api_id = aws_api_gateway_rest_api.test-api-tf.id
+  resource_id = aws_api_gateway_resource.test-api-res-getdata.id
+  http_method = aws_api_gateway_method.get-api.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "getdata-response-integration" {
+  rest_api_id = aws_api_gateway_rest_api.test-api-tf.id
+  resource_id = aws_api_gateway_resource.test-api-res-getdata.id
+  http_method = aws_api_gateway_method.get-api.http_method
+  status_code = aws_api_gateway_method_response.getdata-response_200.status_code
+
+  # Transforms the backend JSON response to XML
+  response_templates = {
+    "application/json" = <<EOF
+    
+    EOF
+  }
+}
--- terraform/lambda-get-role.tf
+++ terraform/lambda-get-role.tf
@@ -17,8 +17,46 @@ resource "aws_iam_role" "lambda-get-role" {
 }
 
 
+
+resource "aws_iam_policy" "cloudwatch-policy" {
+  name   = "tf-lamda-s3-trigger"
+  policy = <<EOF
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": [
+                    "s3:GetObject"
+                ],
+                "Resource": "*"
+            }
+        ]
+    }
+    EOF
+
+}
+
+
 resource "aws_iam_role_policy_attachment" "lambda-get-role-attachment" {
   role       = aws_iam_role.lambda-get-role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
 }
 
+
+resource "aws_iam_role_policy_attachment" "cloudwatch-policy-attachment" {
+
+  role       = aws_iam_role.lambda-get-role.name
+  policy_arn = aws_iam_policy.cloudwatch-policy.arn
+}
+
+# Lambda
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda-get.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  source_arn = "arn:aws:execute-api:${var.region}:${var.accountId}:${aws_api_gateway_rest_api.test-api-tf.id}/*/${aws_api_gateway_method.get-api.http_method}${aws_api_gateway_resource.test-api-res-getdata.path}"
+}
