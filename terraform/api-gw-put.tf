b126ec4461b4d5801cda76c3c8a76b085bdf6356
ravillagopinath <ravilladhaneesh@gmail.com>
2024-12-01 20:30:38 +0530

    Update http method to PUT

--- terraform/api-gw-put.tf
+++ terraform/api-gw-put.tf
@@ -8,7 +8,7 @@ resource "aws_api_gateway_method" "put-data-method" {
   resource_id   = aws_api_gateway_resource.api-put-res.id
   rest_api_id   = aws_api_gateway_rest_api.test-api-tf.id
   authorization = "AWS_IAM"
-  http_method   = "POST"
+  http_method   = "PUT"
 }
 
 resource "aws_api_gateway_integration" "api-gw-put-integration" {
