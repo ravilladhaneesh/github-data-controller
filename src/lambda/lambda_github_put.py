7815b6b307baaf17357ac44fd85b1184cc2061ad
ravilladhaneesh <ravilladhaneesh@gmail.com>
2025-08-07 15:37:40 +0530

    Update put lambda to accept only 5 records per github username

--- src/lambda/lambda_github_put.py
+++ src/lambda/lambda_github_put.py
@@ -2,7 +2,38 @@ import json
 import boto3
 # import requests
 from decimal import Decimal
+import os
 
+dynamodb = boto3.resource('dynamodb', region_name='ap-south-1')
+table = dynamodb.Table('github-repo-data')
+PUT_LIMIT = int(os.environ.get('PUT_LIMIT', 5))
+
+def present_items_of_user(username):
+    response = table.query(
+        KeyConditionExpression=boto3.dynamodb.conditions.Key('username').eq(username))
+    
+    return response.get("Items", {})
+
+
+def put_data_to_dynamodb(event):
+    decimal_languages = languages_to_decimal_type(event["body"]["languages"])
+    username = event["body"]['username']
+    reponame = event["body"]['reponame']
+    
+    print(table)
+    response = table.put_item(
+        Item={
+            'username': username,
+            'repo_name': reponame,
+            "languages": decimal_languages,
+            "repo_url": event["body"]["url"],
+            "branch": event["body"]["branch"],
+            "is_private_repo": event["body"]["is_private"]
+        }
+    )
+    return response
+
+    
 def lambda_handler(event, context):
     
     is_valid = validate_event(event)
@@ -14,42 +45,36 @@ def lambda_handler(event, context):
         }
     
     try:
-        decimal_languages = languages_to_decimal_type(event["body"]["languages"])
-        username = event["body"]['username']
-        reponame = event["body"]['reponame']
-
-        dynamodb = boto3.resource('dynamodb', region_name='ap-south-1')
-        table = dynamodb.Table('github-repo-data')
-
-        response = table.put_item(
-            Item={
-                'username': username,
-                'repo_name': reponame,
-                "languages": decimal_languages,
-                "repo_url": event["body"]["url"],
-                "branch": event["body"]["branch"],
-                "is_private_repo": event["body"]["is_private"]
+        new_repo = True
+        present_items = present_items_of_user(event["body"]["username"])
+        for item in present_items:
+            if item['repo_name'] == event["body"]["reponame"]:
+                new_repo = False
+        if new_repo and len(present_items) >= PUT_LIMIT:
+            return {
+                'statusCode': 429,
+                'message': "Max limit reached"
             }
-        )
         
+        response = put_data_to_dynamodb(event)
         if response['ResponseMetadata']['HTTPStatusCode'] == 200:
 
             return {
                 "statusCode": 200,
-                "body": f"Successfully put Data for username: {username}, repo: {reponame}."
+                "body": f"Successfully put Data for username: {event['body']['username']}, repo: {event['body']['reponame']}."
             }
         else:
-            raise Exception(f"Unable to put data for username: {username}, repo: {reponame}.")
+            raise Exception(f"Unable to put data for username: {event['body']['username']}, repo: {event['body']['reponame']}.")
 
-
+        
     except Exception as e:
         print("Exception: ", e)
         message = f"Exception {e}"
         
-    return {
-            "statusCode": 500,
-            "message": message
-        }
+        return {
+                "statusCode": 500,
+                "message": message
+            }
     
 
 def validate_event(event):
